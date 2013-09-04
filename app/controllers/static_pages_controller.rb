ce5d69679a57be78dd6a05a685cf78944e73fbe0
runup <645112281@qq.com>
2013-09-04 19:13:45 +0800

    initial commit

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,29 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def help
+  end
+  
+  def about
+  end
+
+  def contact
+  end 
+
+  def history
+  end
+
+  def legend
+  end
+  
+	def value
+  end 
+
+  def healthy
+  end
+
+  def article
+  end
+  
+end
